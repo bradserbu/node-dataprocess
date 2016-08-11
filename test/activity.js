ad8d61b1aa072ff7609109fafc855dacb3882fd9
Brad Serbu <bradserbu@bradserbu.com>
2016-08-11 16:32:49 -0400

    Stupid changes.

--- test/activity.js
+++ test/activity.js
@@ -2,13 +2,20 @@
 'use strict';
 
 // ** Dependencies
-const Activity = require('..').Activity;
+const Q = require('q');
+const Activity = require('../lib/Activity');
 
-const say_hello = Activity('say-hello', name => console.log(`Hello, ${name}!`), {
+const say_hello = Activity('say-hello', name => Q.delay(1000).then(`Hello, ${name}!`), {
     logArguments: true
 });
 
 // Run an instance of the say hello activity and then log the stats;
-say_hello
-    .run('Brad Serbu')
-    .then(() => console.log('STATS', say_hello.stats()));
+const instance = say_hello('Brad Serbu');
+
+console.log(instance);
+
+Q
+    .when(instance.completed)
+    .then(instance => console.log(instance));
+
+// console.log('INSTANCE', instance);
