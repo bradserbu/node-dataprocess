80e53b63183d283a82d89268ff1fe72d4f1f83fe
Brad Serbu <bradserbu@bradserbu.com>
2016-09-13 10:44:27 -0400

    Adding back the activities module.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    Activities: require('./src/Activities'),
     Activity: require('./src/Activity'),
     DataProcess: require('./src/DataProcess'),
     HealthCheck: require('./src/HealthCheck')
